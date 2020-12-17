2f5a7abac24b7e0909bbbae640ff4cc49f64b779
Fernanda Pereira <fernandacarolina.fcsp@gmail.com>
2020-12-17 09:01:21 -0300

    maked app reusable

--- django-polls/polls/admin.py
+++ django-polls/polls/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from .models import Question, Choice
 
+
 class ChoiceInLine(admin.TabularInline):
     model = Choice
     extra = 3
+
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
@@ -14,5 +16,6 @@ class QuestionAdmin(admin.ModelAdmin):
     list_display = ('question_text', 'pub_date', 'was_published_recently')
     list_filter = ['pub_date']
     search_fields = ['question_text']
+
 
 admin.site.register(Question, QuestionAdmin)
